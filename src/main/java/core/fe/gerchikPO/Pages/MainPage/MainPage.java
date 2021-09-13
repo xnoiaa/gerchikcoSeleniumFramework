9cc69eb7c14557b9f8be695147a6ceeea2dfd684
Vlad_QA3 <qa3@gerchikco.com>
2021-09-13 18:52:36 +0300

    add PoupupTest

--- src/main/java/core/fe/gerchikPO/Pages/MainPage/MainPage.java
+++ src/main/java/core/fe/gerchikPO/Pages/MainPage/MainPage.java
@@ -18,9 +18,40 @@ public class MainPage extends AbstractPage {
     private List<WebElement> mainPageTitle;
 
 
-
     @FindBy(xpath = "//a[@href=\"/support\"]\n")
     private WebElement supportMenuVerticalPage;
+
+    @FindBy(xpath = "/html/body/div/div[2]/div[1]/div/a[1]/div")
+    private WebElement depositButtonInVertikalMenu;
+
+    @FindBy(xpath = "//*[@id=\"app\"]/div[2]/div[1]/div/a[2]/div/span")
+    private WebElement createSupportMasegeButton;
+
+    @FindBy(xpath = "//*[@id=\"app\"]/div[2]/div[2]/div[3]/div[1]/div[2]/a[1]")
+    private WebElement depositGreenButton;
+
+    @FindBy(xpath = "//*[@id=\"app\"]/div[2]/div[2]/div[3]/div[1]/div[2]/a[2]")
+    private WebElement withdrowalBlueButton;
+
+    @FindBy(xpath = "//*[@id=\"app\"]/div[2]/div[2]/div[3]/div[1]/div[2]/a[3]")
+    private WebElement transactionHistoriButton;
+
+    @FindBy(xpath = "//div[@class='close float--right close--margins']")
+    private WebElement closePopupButton;
+
+    @FindBys({
+            @FindBy(xpath = "//div[@class=\"verification-steps__step flex--row_center-left margin--bottom_20 \"]")
+    })
+    private List<WebElement> verificationSteps;
+
+
+
+
+    @FindBys({
+            @FindBy(xpath = "//div[@class='popup__inner popup__inner--600 paddings--all_20']")
+    })
+    private List<WebElement> popup;
+
 
 
     public MainPage(WebDriver driver) {
@@ -32,9 +63,33 @@ public class MainPage extends AbstractPage {
         return new  MainPageSupport(driver);
     }
 
-
     public void isPresentMainPageTitle(){
         Assert.assertEquals(mainPageTitle.size(),1);
+    }
+
+    private void test (WebElement webElement) {
+        webElement.click();
+        Assert.assertEquals(1, popup.size());
+        closePopupButton.click();
+    }
+
+    private void testOnDisplayedPopup (){
+        Assert.assertEquals(0, popup.size());
+    }
+
+
+    public void depositButtonInVertikalMenuCheck (){
+        test(depositButtonInVertikalMenu);
+    }
+
+    public void depositGreenButtonCheck (){
+        test(depositGreenButton);
+    }
+
+    public void checkVerificationSteps(){
+        for (int i = 1; verificationSteps.size()>i; i++ )    {
+            test(verificationSteps.get(i));
+        }
     }
 
 }
--- src/test/java/MainPageTest.java
+++ src/test/java/MainPageTest.java
@@ -11,4 +11,15 @@ public class MainPageTest extends BaseTest{
         MainPage mainPage = gerchikLoginPage.loginInSystem("qa3@starway-media.com","1111111q");
     }
 
+    @Test//Перевірка на відображення попапу, який відображається при неповній реєстрації. Перевіряємо кнопку Пополнить баланс
+    public void checkPopup (){
+        GerchikLoginPage gerchikLoginPage = new GerchikLoginPage(getWebDriver());
+        getBaseLktUrl();
+        MainPage mainPage = gerchikLoginPage.loginInSystem("qa3@starway-media.com","1111111q");
+
+        mainPage.depositButtonInVertikalMenuCheck();
+        mainPage.depositGreenButtonCheck();
+        mainPage.checkVerificationSteps();
+    }
+
 }
